716425edad23dc1240141fb4f3ec8ee4d5031337
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2020-03-15 03:55:47 +0100

    Add date to action

--- SwiftRexMonitor/Redux/Model/ActionMessage.swift
+++ SwiftRexMonitor/Redux/Model/ActionMessage.swift
@@ -2,9 +2,9 @@ import Foundation
 import SwiftRex
 
 public struct ActionMessage: Codable, Equatable {
+    public let remoteDate: Date
     public let action: String
     public let actionPayload: PayloadTree
     public let state: PayloadTree
     public let actionSource: ActionSource
-
 }
