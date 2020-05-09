e613c5f97d169feda55b83493788e35c5e711f4e
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2020-05-09 22:04:09 +0200

    Root SPM file

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MonitoredAppMiddleware",
+    platforms: [
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
+    ],
+    products: [
+        .library(name: "MonitoredAppMiddleware", targets: ["MonitoredAppMiddleware"])
+    ],
+    dependencies: [
+        .package(name: "MultipeerMiddleware", url: "https://github.com/SwiftRex/MultipeerMiddleware.git", .branch("master"))
+    ],
+    targets: [
+        .target(name: "MonitoredAppMiddleware", dependencies: ["MultipeerMiddleware"], path: "MonitoredAppMiddleware/Sources/MonitoredAppMiddleware"),
+        .testTarget(name: "MonitoredAppMiddlewareTests", dependencies: ["MonitoredAppMiddleware"])
+    ]
+)
